c1800e397566cf94fe00790ae0417ac7cadb348a
Antony Antony <antony@phenome.org>
2026-02-17 11:17:38 +0100

    publishconf.py: detect GitHub Pages and set SITEURL from GITHUB_PAGES env

--- publishconf.py
+++ publishconf.py
@@ -9,8 +9,15 @@ from pelicanconf import *  # noqa: F403
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://linux-ipsec.org"
-RELATIVE_URLS = False
+# When GITHUB_PAGES is set (e.g. "antonyantony/linux-ipsec.org"), use GitHub Pages URL
+GITHUB_PAGES = os.environ.get("GITHUB_PAGES", "")
+if GITHUB_PAGES:
+    owner, repo = GITHUB_PAGES.split("/", 1)
+    SITEURL = f"https://{owner}.github.io/{repo}"
+    RELATIVE_URLS = True
+else:
+    SITEURL = "https://linux-ipsec.org"
+    RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
